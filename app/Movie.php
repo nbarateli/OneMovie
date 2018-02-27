7b4d2a4f015f7c6f543fafa6f7a1ecf2ebba1224
Niko Barateli <nbara15@freeuni.edu.ge>
2018-02-28 01:42:50 +0400

    created models and migrations for movie, genre, cast and role.

---
+++ app/Movie.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    public function genres()
+    {
+        return $this->belongsToMany('App\Genre');
+    }
+
+}

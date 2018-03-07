fcbe4929a623294053aec4a5ecc6cb993bfb32f2
Niko Barateli <nbara15@freeuni.edu.ge>
2018-03-07 19:07:55 +0400

    the country model now provides you with the list of movies associated to it

--- app/Country.php
+++ app/Country.php
@@ -5,5 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model {
+    public function getByName($name) {
+        return $this->where('country_name', $name)->first();
+    }
 
+    public function movies() {
+        return $this->hasMany('App\Movie');
+    }
 }

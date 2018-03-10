5b0987ef0be4027e955032e50b761c57b5df03b6
Niko Barateli <nbara15@freeuni.edu.ge>
2018-03-10 17:35:33 +0400

    the comment model now allows you to retrieve author and the associated movie

--- app/Comment.php
+++ app/Comment.php
@@ -5,5 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model {
-    //
+    public function Author() {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function movie() {
+        return $this->belongsTo('App\Movie', 'movie_id');
+    }
 }

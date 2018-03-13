3efc4415a28d5a3594b2cf4cafd8422633ee062f
Niko Barateli <nbara15@freeuni.edu.ge>
2018-03-13 21:31:41 +0400

    created a view for displaying simple messages

---
+++ resources/views/message.blade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Niko
+ * Date: 13.03.2018
+ * Time: 21:30
+ */ ?>
+
+@section('content')
+    <div class="container">
+        <h3>{{$message}}</h3>
+    </div>
+@endsection

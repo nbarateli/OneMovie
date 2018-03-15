77fcb32ad3b76c90db1645dc2969e35591a86f4c
Niko Barateli <nbara15@freeuni.edu.ge>
2018-03-15 13:58:45 +0400

    moviescontroller now has a method to return all genres

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -15,25 +15,16 @@ function in_range($val, $start, $end) {
 class MoviesController extends Controller {
     const MOVIES_PER_PAGE = 24;
 
+    public function allMovies($page = 1) {
+        return $this->movies_view(Movie::all(), null, 'All movies', $page);
+    }
+
     public function findByGenre($genre_name, $page = 1) {
         $genre = (new Genre())->findByName($genre_name) ? (new Genre())->findByName($genre_name) : Genre::find(1);
 
         $movies = $genre->movies()->orderBy('id')->get();
 
-        $next_page = $prev_page = -1;
-        $n_pages = $this->count_pages($movies, $page, $next_page, $prev_page);
-        $movies_on_page = $this->get_movies_on_page($movies, $page);
-        $pages = $this->get_pages($page, $n_pages);
-        return view('movies',
-            [
-                'movies' => $movies_on_page,
-                'page' => $page,
-                'prev_page' => $prev_page,
-                'next_page' => $next_page,
-                'term' => null,
-                'genre' => $genre->genre_name,
-                'pages' => $pages
-            ]);
+        return $this->movies_view($movies, $genre->genre_name, null, $page);
     }
 
     public function findById($id = 1) {
@@ -80,5 +71,32 @@ class MoviesController extends Controller {
             array_push($countries, $country->country_name);
         }
         return response(json_encode($countries))->header('Content-Type', 'Application/Json');
+    }
+
+    private function movies_view($movies, $genre, $term, $page) {
+
+        $next_page = $prev_page = -1;
+        $n_pages = $this->count_pages($movies, $page, $next_page, $prev_page);
+        $movies_on_page = $this->get_movies_on_page($movies, $page);
+        $pages = $this->get_pages($page, $n_pages);
+        return view('movies',
+            [
+                'movies' => $movies_on_page,
+                'page' => $page,
+                'prev_page' => $prev_page,
+                'next_page' => $next_page,
+                'term' => $term,
+                'genre' => $genre,
+                'pages' => $pages
+            ]);
+    }
+
+    public function allGenres() {
+        $genres = [];
+
+        foreach (Genre::all() as $genre) {
+            array_push($genres, $genre->genre_name);
+        }
+        return response(json_encode($genres))->header('Content-Type', 'Application/Json');
     }
 }
